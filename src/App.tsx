39d2defecd336c3e701094b87501c70ff506bdba
ganesh nikale <ganeshnikale7@gmail.com>
2026-07-11 20:22:50 +0530

    created api folder and alice for import

--- src/App.tsx
+++ src/App.tsx
@@ -1,86 +1,98 @@
 import { useEffect, useState } from 'react'
-
-import { collection, getDocs, query, where } from "firebase/firestore"; 
-import { db } from "./firebase/index.ts";
-import './App.css';
-import type {IProject, Itask} from './interface/index.ts';
+import '@/App.css';
+import type { IProject, Itask } from '@/interface/index.ts';
+import { getProjectsByUserId, getTodosByUserId } from '@/api/index.ts';
 
 function App() {
-  
-let [projects, setProjects] = useState<IProject[]>([]);
-let [tasksDone, setTasksDone] = useState< Itask[] >();
-let [tasksInReview, setTasksInReview] = useState< Itask[] >();
-let [tasksInProgress, setTasksInProgress] = useState< Itask[] >();
 
-const getProjectsByUserId = async (uid:string) => {
-  const q = query(collection(db, "projects"), where("userId", "==", uid));
-
-  const querySnapshot = await getDocs(q);
-  querySnapshot.forEach((doc) => {
-
-    // console.log("Project ID:", doc.id, "=> Data:", doc.data());
-    setProjects((prevProjects) => [...prevProjects, doc.data() as IProject]);
-  });
-};
+  let [projects, setProjects] = useState<IProject[]>([]);
+  let [tasksDone, setTasksDone] = useState<Itask[]>();
+  let [tasksInReview, setTasksInReview] = useState<Itask[]>();
+  let [tasksInProgress, setTasksInProgress] = useState<Itask[]>();
+  let [selectedProject, setSelectedProject] = useState<string>('');
 
 
-const getTodosByUserId = async (uid:string) => {
-  const q = query(collection(db, "todos"), where("userId", "==", uid));
+  async function fetchProjects() {
+    let data = await getProjectsByUserId("105950886150924190679");
+    setProjects((prevVal) => [...prevVal, ...data]);
+  }
 
-  const querySnapshot = await getDocs(q);
-  let sortedTasks: { [key: string]: Itask[] } = {};
-  querySnapshot.forEach((doc) => {
-
-     console.log("Todo ID:", doc.id, "=> Data:", doc.data().status);
-
-     sortedTasks[doc.data().status] = sortedTasks[doc.data().status] || [];
-     sortedTasks[doc.data().status].push(doc.data() as Itask);
-  });
-
-  setTasksDone(sortedTasks['done']);
-  setTasksInProgress(sortedTasks['inProgress']);
-  setTasksInReview(sortedTasks['inReview']);
- 
-  
-};
+  async function fetchTodos(projectId: string) {
+    let data = await getTodosByUserId(projectId);
+    setTasksDone(data['done']);
+    setTasksInReview(data['in-review']);
+    setTasksInProgress(data['in-progress']);
+  }
 
 
-useEffect(() => {
-  
-  getProjectsByUserId("105950886150924190679");
-  getTodosByUserId("105950886150924190679");
-
-}, []);
 
 
-//done inProgress inReview
-  
+
+  useEffect(() => {
+    fetchProjects();
+  }, []);
+
+  useEffect(() => {
+    fetchTodos(selectedProject);
+  }, [selectedProject]);
+
+
+
   return (
-   
-<div>
-    {/* {projects.map((task, index) => (
-      <div key={index}>
-        <p>Project Name: {task.projectName}</p>
-        <p>Project ID: {task.projectId}</p>
-        <p>User ID: {task.userId}</p>
-        <p>Created At: {task.createAt}</p>
-      </div>
-    ))} */}
 
-    {tasksDone && tasksDone.map((task, index) => (
-      <div key={index}>
-        <p>Task Text: {task.text}</p>
-        <p>Task ID: {task.todoId}</p>
-        <p>User ID: {task.userId}</p>
-        <p>Status: {task.status}</p>
-        <p>Description: {task.discription}</p>
-        <p>Project ID: {task.projectId}</p>
-        <p>Created At: {task.createAt}</p>
-      </div>
-    ))}
+    <div>
 
-     
-  </div>
+
+
+      <select value={selectedProject} onChange={(e) => setSelectedProject(e.target.value)}>
+        <option value="">Select a project</option>
+        {projects.map((task, index) => (
+          <option key={index} value={task.projectId}>
+            {task.projectName}
+
+          </option>
+        ))}
+      </select>
+
+      {selectedProject}
+      {tasksDone && tasksDone.map((task, index) => (
+        <div key={index}>
+          <p>Task Text: {task.text}</p>
+          <p>Task ID: {task.todoId}</p>
+          <p>User ID: {task.userId}</p>
+          <p>Status: {task.status}</p>o
+          <p>Description: {task.discription}</p>
+          <p>Project ID: {task.projectId}</p>
+          <p>Created At: {task.createAt}</p>
+        </div>
+      ))}
+
+      {tasksInReview && tasksInReview.map((task, index) => (
+        <div key={index}>
+          <p>Task Text: {task.text}</p>
+          <p>Task ID: {task.todoId}</p>
+          <p>User ID: {task.userId}</p>
+          <p>Status: {task.status}</p>o
+          <p>Description: {task.discription}</p>
+          <p>Project ID: {task.projectId}</p>
+          <p>Created At: {task.createAt}</p>
+        </div>
+      ))}
+
+      {tasksInProgress && tasksInProgress.map((task, index) => (
+        <div key={index}>
+          <p>Task Text: {task.text}</p>
+          <p>Task ID: {task.todoId}</p>
+          <p>User ID: {task.userId}</p>
+          <p>Status: {task.status}</p>o
+          <p>Description: {task.discription}</p>
+          <p>Project ID: {task.projectId}</p>
+          <p>Created At: {task.createAt}</p>
+        </div>
+      ))}
+
+
+    </div>
 
 
   )
---
+++ src/api/index.ts
@@ -0,0 +1,30 @@
+import { collection, getDocs, query, where } from "firebase/firestore"; 
+import { db } from "@/firebase/index.ts";
+
+import type {IProject, Itask} from '@/interface/index.ts';
+
+
+export const getProjectsByUserId = async (uid:string) => {
+  const q = query(collection(db, "projects"), where("userId", "==", uid));
+
+  const querySnapshot = await getDocs(q);
+  return querySnapshot.docs.map((doc) => doc.data() as IProject);
+};
+
+
+export const getTodosByUserId = async (uid:string) => {
+  const q = query(collection(db, "todos"), where("projectId", "==", uid));
+
+  const querySnapshot = await getDocs(q);
+  let sortedTasks: { [key: string]: Itask[] } = {};
+   querySnapshot.forEach((doc) => {
+
+     sortedTasks[doc.data().status] = sortedTasks[doc.data().status] || [];
+     sortedTasks[doc.data().status].push(doc.data() as Itask);
+  });
+
+  return sortedTasks;
+  
+};
+
+
